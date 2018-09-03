84f9657a46e8ba9a44075275dd284b5ecfb27311
kkjasoncheung <kkjason.cheung@mail.utoronto.ca>
2018-09-03 00:55:21 -0400

    Uses angular material tool bar component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { MatToolbarModule } from '@angular/material';
+
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ListComponent } from './components/list/list.component';
@@ -13,7 +15,7 @@ const routes: Routes = [
   { path: 'create', component: CreateComponent },
   { path: 'edit/:id', component: EditComponent },
   { path: 'list', component: ListComponent },
-  { path: '', redirectTo: 'list', pathMatch: 'full'}
+  { path: '', redirectTo: 'list', pathMatch: 'full' }
 ];
 
 @NgModule({
@@ -27,7 +29,8 @@ const routes: Routes = [
     BrowserModule,
     BrowserAnimationsModule,
     // Configure the router module for root
-    RouterModule.forRoot(routes) 
+    RouterModule.forRoot(routes),
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
