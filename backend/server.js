8ea248711d8e9412a6fdea45678abbe239512877
kkjasoncheung <kkjason.cheung@mail.utoronto.ca>
2018-09-03 13:57:20 -0400

    Adds endpoints for creating and clearing cookies

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,24 @@
+var cookieParser = require('cookie-parser');
 var express = require('express');
 
-const app = express();
+var app = express();
+app.use(cookieParser());
 
 // Default request
 app.get('/', (req, res) => {
     res.send('Hello, World');
+});
+
+// Route to set cookie
+app.get('/cookies', (req, res) => {
+    var cookie_name = 'login';
+    res.cookie(cookie_name, 'cookie_value', {maxAge: 9999999}).send('cookie is set');
+});
+
+// Route to clear cookies
+app.get('/clearcookies', (req, res) => {
+    res.clearCookie('login');
+    res.send('cookie deleted');
 });
 
 // Event listener
