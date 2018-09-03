34109df79aa6fd0b743721efa351a5ac16600c66
kkjasoncheung <kkjason.cheung@mail.utoronto.ca>
2018-09-03 14:14:03 -0400

    Adds body-parser dependency and sets up mongoose in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,31 @@
+"use strict";
 var cookieParser = require('cookie-parser');
 var express = require('express');
+var cors = require('cores');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var app = express();
+
+// Create modular routing
+const router = express.Router();
+
+// use middleware
+app.use(cors());
+app.use(bodyParser.json());
 app.use(cookieParser());
 
-// Default request
-app.get('/', (req, res) => {
-    res.send('Hello, World');
+// connect to mongoDB with URL. TODO: Set up MongoDB instance to connect to
+mongoose.connect('');
+
+const connection = mongoose.connection;
+
+// Event listener for opening connection to DB
+connection.once('open', () => {
+    console.log('Connection to MongoDB has been established successfully');
 });
+
+app.use('/', router);
 
 // Route to set cookie
 app.get('/cookies', (req, res) => {
