0097e550fdbd8042f0aec4cf5baa5a2c38bb8cc1
kkjasoncheung <kkjason.cheung@mail.utoronto.ca>
2018-09-03 14:32:11 -0400

    Adds routes for querying all issues, issue by id, adding issue to db

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ var express = require('express');
 var cors = require('cores');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var Issue = require('./models/issue');
 
 var app = express();
 
@@ -25,12 +26,45 @@ connection.once('open', () => {
     console.log('Connection to MongoDB has been established successfully');
 });
 
+// Route to find all issues
+router.route('/issues').get((req, res) => {
+    Issue.find((err, issues) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(issues);
+        }
+    })
+});
+
+// Route to find issue by id
+router.route('/issues/:id').get((req, res) => {
+    Issue.findById(req.params.id, (err, issue) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(issue);
+        }
+    });
+});
+
+// Route to add issue to database
+router.route('/issues/add').post((req, res) => {
+    // Create a new issue with data already in body
+    let issue = new Issue(req.body);
+    issue.save().then((issue) => {
+        res.status(200).json({'issue': 'Added successfully'});
+    }).catch((err) => {
+        res.status(400).send('Failed to create new record');
+    });
+});
+
 app.use('/', router);
 
 // Route to set cookie
 app.get('/cookies', (req, res) => {
     var cookie_name = 'login';
-    res.cookie(cookie_name, 'cookie_value', {maxAge: 9999999}).send('cookie is set');
+    res.cookie(cookie_name, 'cookie_value', { maxAge: 9999999 }).send('cookie is set');
 });
 
 // Route to clear cookies
